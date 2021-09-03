97889bd0038a9e6ee1aa4b64f0069ac32dcf7c31
takenori-sasa <yoshida.tkw@gmail.com>
2021-09-03 15:01:06 +0900

    Boardパート変えた

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import { Repeat } from "typescript-tuple"; // for Board
 // Squareパート:一個の四角だけ担当
 type SquareState = "O" | "X" | null;
 type SquareProps = {
@@ -13,36 +14,38 @@ const Square = (props: SquareProps) => (
   </button>
 );
 
-class Board extends React.Component {
-  renderSquare(i: number) {
-    return <Square />;
-  }
+// Boardパート:Square9つ分
+type BoardState = Repeat<SquareState, 9>;
+type BoardProps = {
+  squares: BoardState;
+  onClick: (i: number) => void;
+};
 
-  render() {
-    const status = "Next player: X";
+const Board = (props: BoardProps) => {
+  const renderSquare = (i: number) => (
+    <Square value={props.squares[i]} onClick={() => props.onClick(i)} />
+  );
 
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+  return (
+    <div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
-    );
-  }
-}
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+};
 
 class Game extends React.Component {
   render() {
